b02cc86e8a58c723478820960db8fda77f17aae7
dimitrisna <dnn0363@gmail.com>
2024-09-02 10:19:40 +0000

    Add managerial endpoints

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from fastapi import FastAPI
-from routers import topics,authorization,projects,send_data,cassandra_queries
+from routers import topics,authorization,projects,send_data,cassandra_queries,managerial
 
 from dependencies import get_current_user
 
@@ -12,3 +12,4 @@ app.include_router(projects.router)
 app.include_router(send_data.router)
 app.include_router(cassandra_queries.router)
 app.include_router(topics.router)
+app.include_router(managerial.router)
