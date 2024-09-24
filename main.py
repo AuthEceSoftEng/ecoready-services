119bd2b1d70a4a4ba383024bf8513bd8d4a27353
Dimitris Nastos <dnn0363@gmail.com>
2024-09-24 09:07:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,4 @@ from dependencies import get_current_user
 
 app = FastAPI()
 
-
-app.include_router(authorization.router)
-app.include_router(projects.router)
-app.include_router(send_data.router)
-app.include_router(cassandra_queries.router)
-app.include_router(topics.router)
 app.include_router(managerial.router)
